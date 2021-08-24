891c6ea0f8037415d4447f2803ec73947dd9b7cc
ganudoomer <ganuiscool@gmail.com>
2021-08-24 22:08:22 +0530

    Initial Commit

---
+++ War-game/War_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_gameApp.swift
+//  War-game
+//
+//  Created by Sree on 24/08/21.
+//
+
+import SwiftUI
+
+@main
+struct War_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
